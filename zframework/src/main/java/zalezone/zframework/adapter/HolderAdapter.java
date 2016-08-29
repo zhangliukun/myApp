f485341a8d3ac30b312e6be1d09bc22e0307403f
zhangliukun <1433317488@qq.com>
2016-08-29 16:36:10 +0800

    commit1

--- zframework/src/main/java/zalezone/zframework/adapter/HolderAdapter.java
+++ zframework/src/main/java/zalezone/zframework/adapter/HolderAdapter.java
@@ -1,7 +1,15 @@
 package zalezone.zframework.adapter;
 
+import android.content.Context;
+
+import java.util.List;
+
 /**
  * Created by zale on 16/8/29.
  */
-public class HolderAdapter {
+public class HolderAdapter<T> extends AbstractAdapter<T> {
+
+    public HolderAdapter(Context context, List<T> listData) {
+        super(context, listData);
+    }
 }
