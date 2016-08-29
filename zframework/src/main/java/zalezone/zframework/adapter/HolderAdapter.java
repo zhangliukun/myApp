1c6ea36d072ba2182febe7e20fd7ce4520c2f320
zhangliukun <1433317488@qq.com>
2016-08-29 16:42:13 +0800

    commit2

--- zframework/src/main/java/zalezone/zframework/adapter/HolderAdapter.java
+++ zframework/src/main/java/zalezone/zframework/adapter/HolderAdapter.java
@@ -12,6 +12,8 @@ public class HolderAdapter<T> extends AbstractAdapter<T> {
     public HolderAdapter(Context context, List<T> listData) {
         super(context, listData);
         String commit2;
+        String commit3;
+        String commit4;
     }
 
 
